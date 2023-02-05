4acaa4ccfd4289762642e9e941b8a01703cd7c88
inedumozey <inedumozey@gmail.com>
2023-02-04 16:53:07 -0800

    updated commit

--- components/landing/index.jsx
+++ components/landing/index.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 export default function Landing() {
     return (
         <div>
-            index
-            <br />
-            {process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}
-            <br />
-            {process.env.NEXT_PUBLIC_BACKEND_BASE_URL}
+            Welcome to Ekovit!
         </div>
     )
 }
