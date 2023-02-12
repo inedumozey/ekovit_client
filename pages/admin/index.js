5a5aec658446cca787e00f095569b4f58ff9e630
inedumozey <inedumozey@gmail.com>
2023-02-12 15:29:31 -0800

    updated

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Spinner from '../../utils/components/Spinner'
 
-export default function index() {
+export default function Index() {
     const router = useRouter()
 
     useEffect(() => {
